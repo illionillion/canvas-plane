663b58d1940d1b324132a0b2b73af6403539a675
yuto ichinose <yuto.0526.sano@gmail.com>
2023-06-30 00:52:54 +0900

    歩くアニメーション

--- src/js/main.js
+++ src/js/main.js
@@ -6,33 +6,34 @@ const main = () => {
   const ctx = canvas.getContext("2d");
 
   // 棒人間の描画
-  function drawStickFigure(x, y, flag = false) {
+  function drawStickFigure(x, y, frame) {
+    const legOffset = Math.sin(frame / 10) * 10;
+
     // 頭
     ctx.beginPath();
-    ctx.arc(x, y, 20, 0, Math.PI * 2);
+    ctx.arc(x, y - 60, 20, 0, Math.PI * 2);
     ctx.fillStyle = "black";
     ctx.fill();
 
     // 体
-    ctx.moveTo(x, y + 20);
-    ctx.lineTo(x, y + 80);
+    ctx.moveTo(x, y - 40);
+    ctx.lineTo(x, y + 20);
 
     // 左腕
-    ctx.moveTo(x, y + 40);
-    ctx.lineTo(x - (flag ? 40 : 45), y + (flag ? 60 : 45));
-    
+    ctx.moveTo(x, y - 20);
+    ctx.lineTo(x - 40, y + 25 - legOffset);
+
     // 右腕
-    ctx.moveTo(x, y + 40);
-    ctx.lineTo(x + (flag ? 40 : 45), y + (flag ? 60 : 45));
-    // ctx.lineTo(x + 40, y + 60);
+    ctx.moveTo(x, y - 20);
+    ctx.lineTo(x + 40, y + 25 + legOffset);
 
     // 左足
-    ctx.moveTo(x, y + 80);
-    ctx.lineTo(x - 30, y + 120);
+    ctx.moveTo(x, y + 20);
+    ctx.lineTo(x - 30, y + 80 + legOffset);
 
     // 右足
-    ctx.moveTo(x, y + 80);
-    ctx.lineTo(x + 30, y + 120);
+    ctx.moveTo(x, y + 20);
+    ctx.lineTo(x + 30, y + 80 - legOffset);
 
     ctx.strokeStyle = "black";
     ctx.lineWidth = 5;
@@ -44,32 +45,31 @@ const main = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
 
-  // 飛行機の初期位置
-  let airplaneX = canvas.width / 2;
-  let airplaneY = canvas.height / 2;
+  // 棒人間の初期位置
+  let stickFigureX = canvas.width / 2;
+  let stickFigureY = canvas.height / 2;
 
-  let animationFlag = false;
+  // アニメーションのフレーム数と速度
+  let frame = 0;
+  const speed = 3;
 
-  setInterval(() => {
-    animationFlag = !animationFlag;
-    clearCanvas();
-    drawStickFigure(airplaneX, airplaneY, animationFlag);
-  }, 500);
-
-  // 飛行機の描画と位置の更新
+  // 棒人間の描画と位置の更新
   function update() {
     clearCanvas();
+    drawStickFigure(stickFigureX, stickFigureY, frame);
 
     // 位置の更新
-    airplaneX += 1;
-    airplaneY += 1;
+    // stickFigureX += speed;
+
+    // フレーム数の更新
+    frame++;
 
     // アニメーションのループ
     requestAnimationFrame(update);
   }
 
   // アニメーションの開始
-  //   update();
+  update();
 };
 
 window.addEventListener("load", main);
